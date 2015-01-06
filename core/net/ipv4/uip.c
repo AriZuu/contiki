e17e74854058148bef4b72690b88d35221ba5000
Ari Suutari <ari@stonepile.fi>
2015-01-06 20:09:51 +0200

    Use correct header length for UDP.

--- core/net/ipv4/uip.c
+++ core/net/ipv4/uip.c
@@ -1212,7 +1212,10 @@ uip_process(uint8_t flag)
   uip_ipaddr_copy(&BUF->srcipaddr, &uip_hostaddr);
   uip_ipaddr_copy(&BUF->destipaddr, &uip_udp_conn->ripaddr);
 
-  uip_appdata = &uip_buf[UIP_LLH_LEN + UIP_IPTCPH_LEN];
+/*
+ * Pico]OS: Use correct header length (UDP).
+ */
+  uip_appdata = &uip_buf[UIP_LLH_LEN + UIP_IPUDPH_LEN];
 
 #if UIP_UDP_CHECKSUMS
   /* Calculate UDP checksum. */
