e318973eb58a5c4e9ba3b2875d2dded5847bbbf9
Ari Suutari <ari@stonepile.fi>
2015-01-06 20:09:50 +0200

    Adapt debug output to Pico]OS. Use nosPrintf instead of stdio.

--- core/net/ip/uip-debug.h
+++ core/net/ip/uip-debug.h
@@ -42,7 +42,6 @@
 #define UIP_DEBUG_H
 
 #include "net/ip/uip.h"
-#include <stdio.h>
 
 void uip_debug_ipaddr_print(const uip_ipaddr_t *addr);
 void uip_debug_lladdr_print(const uip_lladdr_t *addr);
@@ -52,30 +51,21 @@ void uip_debug_lladdr_print(const uip_lladdr_t *addr);
 #define DEBUG_ANNOTATE  2
 #define DEBUG_FULL      DEBUG_ANNOTATE | DEBUG_PRINT
 
+/*
+ * Pico]OS: Use nosPrintf instead of printf.
+ */
+
 /* PRINTA will always print if the debug routines are called directly */
-#ifdef __AVR__
-#include <avr/pgmspace.h>
-#define PRINTA(FORMAT,args...) printf_P(PSTR(FORMAT),##args)
-#else
-#define PRINTA(...) printf(__VA_ARGS__)
-#endif
+#define PRINTA(...) nosPrintf(__VA_ARGS__)
 
 #if (DEBUG) & DEBUG_ANNOTATE
-#ifdef __AVR__
-#define ANNOTATE(FORMAT,args...) printf_P(PSTR(FORMAT),##args)
-#else
-#define ANNOTATE(...) printf(__VA_ARGS__)
-#endif
+#define ANNOTATE(...) nosPrintf(__VA_ARGS__)
 #else
 #define ANNOTATE(...)
 #endif /* (DEBUG) & DEBUG_ANNOTATE */
 
 #if (DEBUG) & DEBUG_PRINT
-#ifdef __AVR__
-#define PRINTF(FORMAT,args...) printf_P(PSTR(FORMAT),##args)
-#else
-#define PRINTF(...) printf(__VA_ARGS__)
-#endif
+#define PRINTF(...) nosPrintf(__VA_ARGS__)
 #define PRINT6ADDR(addr) uip_debug_ipaddr_print(addr)
 #define PRINTLLADDR(lladdr) uip_debug_lladdr_print(lladdr)
 #else
